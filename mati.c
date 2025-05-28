d8a398771813439066a56d1a1c78285705767bd3
marcianobeat <marcianobeat7@gmail.com>
2025-05-28 02:17:19 +0000

    mate2

--- mati.c
+++ mati.c
@@ -14,6 +14,14 @@ int main(){
     printf ("Mutiplicação = %d \n", mutiplicacao);
     printf ("Divição = %f \n", divicao);
 
+    long int compara1 = soma > mutiplicacao;
+    unsigned int compara2 = soma < mutiplicacao;
+    long long int compara3 = soma >= mutiplicacao;
+    long double compara4 = soma <= mutiplicacao;
+    unsigned long int compara5 = soma == mutiplicacao;
+    short int compara6 = soma != mutiplicacao;
+ 
+    printf (" %ld \n %u \n %lld \n %lf \n %lu \n %d \n ",compara1, compara2, compara3, compara4, compara5, compara6);
 
-
+    return 0;
 }
