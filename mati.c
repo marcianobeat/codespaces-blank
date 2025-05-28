0a9a12d423be497cef3183b9a7d89e9c0157ea8a
marcianobeat <marcianobeat7@gmail.com>
2025-05-28 01:32:04 +0000

    first commit

---
+++ mati.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+    int b = 5;
+
+    int subtrasao = a - b;
+    int soma = a + b;
+    int mutiplicacao = a * b;
+    float divicao = a / b;
+
+    printf ("Soma = %d \n", soma);
+    printf ("Subitração = %d \n", subtrasao);
+    printf ("Mutiplicação = %d \n", mutiplicacao);
+    printf ("Divição = %f \n", divicao);
+
+
+
+}
