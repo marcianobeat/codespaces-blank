b16b41858da454645cf08730ec4a929f09ada38d
marcianobeat <marcianobeat7@gmail.com>
2025-06-09 23:15:56 +0000

    teste

--- repetiçao.c
+++ repetiçao.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 int main(){
-    int i;
+    int i = 0;
+    int m = 0;
+    int g = 0;
 
     for (i = 0; i < 5; i++) {
         printf ("direita\n");
-
-
-        
     }
+
+    while (m <= 10 ){
+    printf ("encima\n");
+    m++;
+    }
+
+    do { 
+        printf("baixo\n");
+        g++;
+
+    } while (g <=5);
+    
+    
+
+
+    
 
 
 
